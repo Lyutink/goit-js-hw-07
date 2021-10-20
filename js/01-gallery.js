eb2380162018b66d606ecb5befd7d25fe7911aaa
Lyutink <krivenko.lyuda@gmail.com>
2021-10-20 20:15:05 +0300

    Update HW-07-z1.js
    Нужно ещё реализовать galleryRef.removeEventListener('keydown', onClickEscape);  через onClose при закрытие кликом

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,54 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+galleryRef.addEventListener('click', onImgClickOpenModal);
+
+
+const cardsMarkup = makeImgCards(galleryItems);
+galleryRef.insertAdjacentHTML("afterbegin", cardsMarkup);
+
+function makeImgCards(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+        .join('');
+}
+
+
+function onImgClickOpenModal(event) {
+  event.preventDefault();
+
+  galleryRef.addEventListener('keydown', onClickEscape);
+  function onClickEscape(event) {
+      if (event.key !== "Escape") {
+        return;
+      }
+      instance.close();
+      galleryRef.removeEventListener('keydown', onClickEscape);
+}
+
+  const selectedImg = event.target.dataset.source; 
+  const instance = basicLightbox.create(`
+      <div class="modal">
+          <img src="${selectedImg}">
+          <a>Close</a>
+      </div>
+  `, {
+      onShow: (instance) => {
+      instance.element().querySelector('img').onclick = instance.close
+    }
+  })
+  
+  instance.show();
+}
