9c7c792574d86cb3407df11e63743b16f0412fcf
Lyutink <krivenko.lyuda@gmail.com>
2021-10-23 16:10:17 +0300

    Update HW-07_2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,8 +3,6 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 const galleryRef = document.querySelector('.gallery');
-galleryRef.addEventListener('click', onImgClickOpenModal);
-
 
 const cardsMarkup = makeImgCards(galleryItems);
 galleryRef.insertAdjacentHTML("afterbegin", cardsMarkup);
@@ -12,19 +10,18 @@ galleryRef.insertAdjacentHTML("afterbegin", cardsMarkup);
 function makeImgCards(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
         return `<a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" />
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
 </a>`;
     })
         .join('');
 }
 
-function onImgClickOpenModal(event) {
-    event.preventDefault();
-
-    let lightbox = new SimpleLightbox('.gallery a');
-    console.log(lightbox);
-}
+let lightbox = new SimpleLightbox('.gallery a', {
+    'captions': true,
+    'captionDelay': 250,       
+});
 
 
-///////
+
+
 
